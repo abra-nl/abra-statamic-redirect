823522b1b8bcd1e3c43e96eb88f2036ad39ef461
Mike Slinkman <mike@abra.nl>
2026-01-14 18:23:31 +0100

    feat: implement basic create / edit pages

---
+++ resources/js/pages/Create.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Button, Card, Header } from '@statamic/cms/ui';
+import RedirectForm from "../components/RedirectForm.vue";
+
+const props = defineProps({
+  statusCodes: Object,
+});
+
+</script>
+
+<template>
+  <div>
+    <Header title="Create redirect">
+      <Button text="Back to list" variant="primary" :href="cp_url('redirects')" />
+    </Header>
+    <Card>
+      <RedirectForm
+        :action="cp_url('redirects/store')"
+        :submit-text="'Create redirect'"
+        :status-codes="statusCodes"
+        :redirect="null"
+      />
+    </Card>
+  </div>
+</template>
---
+++ resources/js/pages/Edit.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Button, Card, Header } from '@statamic/cms/ui';
+import RedirectForm from "../components/RedirectForm.vue";
+
+const props = defineProps({
+  statusCodes: Object,
+  redirect: Array
+});
+
+</script>
+
+<template>
+  <div>
+    <Header title="Edit redirect">
+      <Button text="Back to list" variant="primary" :href="cp_url('redirects')" />
+    </Header>
+    <Card>
+      <RedirectForm
+          :action="cp_url(`redirects/${redirect['id']}`)"
+          :submit-text="'Create redirect'"
+          :status-codes="statusCodes"
+          :redirect="redirect"
+      />
+    </Card>
+  </div>
+</template>
